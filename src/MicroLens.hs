c666bd57e76e86a60eae32f92afa0fed6dc71cf1
Kamirus <listopadkamil@gmail.com>
2020-01-18 15:55:55 +0100

    lensmaker kata done

--- src/MicroLens.hs
+++ src/MicroLens.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE RankNTypes, DeriveFunctor, TupleSections #-}
+{-# LANGUAGE RankNTypes, DeriveFunctor, TupleSections, ScopedTypeVariables, MonomorphismRestriction #-}
 
 module MicroLens where
 
 import           Prelude                 hiding ( sum )
-import           Data.Semigroup
+import           Data.Monoid
 import           Control.Applicative
+import           Data.Tuple
 import qualified Data.Traversable              as T
 
 ---------------------------------------------------------
@@ -58,6 +59,8 @@ type Optic p f s t a b = p a (f b) -> p s (f t)
 
 type Iso s t a b = forall p f . (Profunctor p, Functor f) => Optic p f s t a b
 
+type ASetter s t a b = Optic (->) Id s t a b
+
 type Lens s t a b = forall f . Functor f => Optic (->) f s t a b
 
 type Traversal s t a b = forall f . Applicative f => Optic (->) f s t a b
@@ -79,18 +82,32 @@ _2 :: Lens (x, a) (x, b) a b
 _2 f (x, a) = (x, ) <$> f a
 
 -- | A function which takes a lens and looks through it.
-view :: Lens s t a b -> (s -> a)
+-- view :: Lens s t a b -> (s -> a)
+view :: Optic (->) (K a) s t a b -> (s -> a)
 view l = getK . l K
 
 -- | A function which takes a lens and a transformation function
 -- and applies that transformer at the focal point of the lens.
-over :: Lens s t a b -> (a -> b) -> (s -> t)
+-- over :: Lens s t a b -> (a -> b) -> (s -> t)
+over :: Optic (->) Id s t a b -> (a -> b) -> (s -> t)
 over l f = getId . l (Id . f)
 
 -- | A function from a lens and a value which sets the value
 -- at the focal point of the lens. 
-set :: Lens s t a b -> b -> (s -> t)
+-- set :: Lens s t a b -> b -> (s -> t)
+set :: Optic (->) Id s t a b -> b -> (s -> t)
 set l b = getId . l (Id . const b)
+
+(+~) :: Num a => Lens s t a a -> a -> s -> t
+l +~ a = over l (+ a)
+
+-- | Modifies the structure and returns it along with the new value:
+(<%~) :: Optic (->) ((,) b) s t a b -> (a -> b) -> s -> (b, t)
+l <%~ f = l $ (,) <$> f <*> f -- \a -> (f a, f a)
+
+-- | Modifies the structure and returns it along with the old value:
+(<<%~) :: Optic (->) ((,) a) s t a b -> (a -> b) -> s -> (a, t)
+l <<%~ f = l $ (,) <$> id <*> f -- \a -> (a, f a)
 
 ---------------------------------------------------------
 
@@ -104,48 +121,63 @@ elements = traverse
 -- element it focuses on in order.
 -- 
 -- @
--- toListOf :: Optic (->) (K (Endo [a])) s s a a -> (s -> [a])
+-- toListOf :: Traversal s s a a -> (s -> [a])
 -- @
-toListOf :: Traversal s s a a -> (s -> [a])
-toListOf tl = getK . tl f
+toListOf :: Optic (->) (K (Endo [a])) s s a a -> (s -> [a])
+toListOf tl = flip appEndo [] . getK . tl f
  where
-  f :: a -> K [a] a
-  f = K . (: [])
+  f a = K $ Endo $ (:) a
 
 -- | A function which takes any kind of Optic which might
 -- be focused on zero subparts and returns Just the first
 -- subpart or else Nothing.
 --
 -- @
--- preview :: Optic (->) (K (First a)) s s a a -> (s -> Maybe a)
+-- preview :: Traversal s s a a -> (s -> Maybe a)
 -- @
-preview :: Traversal s s a a -> (s -> Maybe a)
-preview tl = fmap getFirst . getOption . getK . tl f
-  where
-    f :: a -> K (Option (First a)) a
-    f = K . Option . Just . First
+preview :: Optic (->) (K (First a)) s s a a -> (s -> Maybe a)
+preview tl = getFirst . getK . tl f
+ where
+  f :: a -> K (First a) a
+  f = K . First . Just
 
 ---------------------------------------------------------
 
 -- | A helper function which witnesses the fact that any
 -- container which is both a Functor and a Contravariant
 -- must actually be empty.
-coerce :: (Contravariant f, Functor f) => f a -> f b
-coerce = error "todo"
+coerce :: forall f a b . (Contravariant f, Functor f) => f a -> f b
+coerce = contramap f . fmap f where f = const ()
+
+type Fold_ s a
+  = forall f . (Contravariant f, Applicative f) => (a -> f a) -> (s -> f s)
 
 -- | A Fold which views the result of a function application
 to :: (a -> b) -> Fold a b
-to f = error "todo"
+to a_b b_fb = coerce . b_fb . a_b
+
+---------------------------------------------------------
+
+type Prism_ s t a b
+  = forall p f . (Choice p, Applicative f) => p a (f b) -> p s (f t)
 
 -- | A prism which focuses on the left branch of an Either
-_Left :: Prism (Either a x) (Either b x) a b
-_Left = error "todo"
+-- p a (f b) -> p (Either a x) (f (Either b x))
+-- p a (f b) -> p (Either a x) (Either (f b) x)
+_Left :: forall a b x . Prism (Either a x) (Either b x) a b
+_Left = rmap (either (fmap Left) (pure . Right)) . left'
 
 -- | A prism which focuses on the right branch of an Either
 _Right :: Prism (Either x a) (Either x b) a b
-_Right = error "todo"
+_Right = rmap (either (pure . Left) (fmap Right)) . right'
 
+---------------------------------------------------------
+
+type Iso_ s t a b
+  = forall p f . (Profunctor p, Functor f) => p a (f b) -> p s (f t)
+
+-- p (b, a) (f (b, a)) -> p (a, b) (f (a, b))
 -- | An iso which witnesses that tuples can be flipped without
 -- losing any information
 _flip :: Iso (a, b) (a, b) (b, a) (b, a)
-_flip = error "todo"
+_flip = dimap swap $ fmap swap
--- test/MicroLensSpec.hs
+++ test/MicroLensSpec.hs
@@ -4,9 +4,7 @@ import           Prelude                 hiding ( sum )
 import           MicroLens
 import           Data.Monoid
 import           Test.Hspec
-import qualified Control.Lens as L
-
-problematic = L.toListOf (L.traversed . L.to succ) [1, 2, 3]
+import qualified Control.Lens                  as L
 
 spec = do
   describe "Basic lensing" $ do
@@ -14,12 +12,14 @@ spec = do
     it "should view _2" $ shouldBe (view _2 (1, 2)) 2
     it "should set _1" $ shouldBe (set _1 0 (1, 2)) (0, 2)
     it "should set _2" $ shouldBe (set _2 0 (1, 2)) (1, 0)
+    it "should add" $ shouldBe (11, 2) $ _1 +~ 10 $ (1, 2)
+    it "should return old integer" $ shouldBe (6, 7) $ ($) <<%~ (+ 1) $ 6
   describe "Basic traversals" $ do
     it "should traverse" $ shouldBe (toListOf elements [1, 2, 3]) [1, 2, 3]
     it "should miss" $ shouldBe (preview elements []) (Nothing :: Maybe Int)
-  -- describe "Basic mapping" $ it "should map" $ shouldBe
-  --   (toListOf (elements . to succ) [1, 2, 3])
-  --   [2, 3, 4 :: Int]
+  describe "Basic mapping" $ it "should map" $ shouldBe
+    (toListOf (elements . to succ) [1, 2, 3])
+    [2, 3, 4 :: Int]
   describe "Basic prisming" $ do
     it "should get Left"
       $ shouldBe (preview _Left (Left 3)) (Just 3 :: Maybe Int)
